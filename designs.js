120af10a0c2c4f575be7fbcfa5f0c34ee36e9a5a
Mira85 <67718364+Mira85@users.noreply.github.com>
2020-07-02 17:30:11 -0400

    code review changes

--- designs.js
+++ designs.js
@@ -6,23 +6,21 @@ function makeGrid(event) {
   // Select size input
   let height = document.getElementById('inputHeight').value;
   let width = document.getElementById('inputWidth').value;
-  // Select color input
-  let color = document.getElementById('colorPicker').value;
 
   let table = document.getElementById('pixelCanvas');
   let tableBody = document.createElement('tbody');
 
+  // to clear out previous grid
   table.innerHTML = "";
   // formation of grid
   for (let r = 0; r < height; r++) {
-
     let row = document.createElement("tr");
 
     for (let c = 0; c < width; c++) {
-
       let cell = document.createElement("td");
       // when cell is clicked by user, background color changes
       cell.addEventListener('click', function (event) {
+        let color = document.getElementById('colorPicker').value;
         event.target.style.backgroundColor = color;
       } );
       row.appendChild(cell);
